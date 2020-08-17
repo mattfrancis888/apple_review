717095728b2fec5418854575f14e7ea7c6e5f20e
Matthew Francis <mattfrancis888@gmail.com>
2020-08-17 14:52:37 -0400

    Created responsive layout for Body component's screenshot section.

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 import appleApp from "../img/appleApp.png";
+import iphoneScreenshot1 from "../img/iphoneScreenshot1.png";
+import iphoneScreenshot2 from "../img/iphoneScreenshot2.png";
+import iphoneScreenshot3 from "../img/iphoneScreenshot3.png";
+import iphoneScreenshot4 from "../img/iphoneScreenshot4.png";
 
 const Body: React.FC<{}> = (props) => {
     return (
@@ -15,9 +19,8 @@ const Body: React.FC<{}> = (props) => {
                     </p>
                 </div>
                 <div className="appContainer">
-                    <div className="appImageContainer">
-                        <img src={appleApp} alt="app icon" />
-                    </div>
+                    <img className="appImage" src={appleApp} alt="app icon" />
+
                     <div className="appInfoContainer">
                         <h1 className="appTitle">Apple Store</h1>
                         <h2 className="appShortDesc">
@@ -25,6 +28,15 @@ const Body: React.FC<{}> = (props) => {
                         </h2>
                         <h2 className="appCompany">Apple</h2>
                         <h2 className="appRank">#53 in Shopping</h2>
+                    </div>
+                </div>
+                <div className="screenshotsPreview">
+                    <h1>Screenshots</h1>
+                    <div className="screenshotsContainer">
+                        <img src={iphoneScreenshot1} alt="screenshot 1" />
+                        <img src={iphoneScreenshot2} alt="screenshot 2" />
+                        <img src={iphoneScreenshot3} alt="screenshot 3" />
+                        <img src={iphoneScreenshot4} alt="screenshot 4" />
                     </div>
                 </div>
             </div>
